18fc120aa87b88a2a3b23b510e3f1994b11dd0cb
dillonl <dillonl49@163.com>
2023-03-23 23:50:06 +0800

    fix:#✨项目初始化

---
+++ src/main/java/com/example/sunset/SunsetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunsetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sunset/SunsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
