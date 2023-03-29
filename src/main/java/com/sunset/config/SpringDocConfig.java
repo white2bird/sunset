99225c0fbe8bf9e689d7e6588f9659fb887de6a1
dillonl <dillonl49@163.com>
2023-03-29 23:19:52 +0800

    FIX:#修改swagger只读说明

--- src/main/java/com/sunset/config/SpringDocConfig.java
+++ src/main/java/com/sunset/config/SpringDocConfig.java
@@ -1,12 +1,10 @@
 package com.sunset.config;
-
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class SpringDocConfig {
     @Bean
