7cbdeef6193042afcd9a51dd083a78c175fb3be9
dillonl <dillonl49@163.com>
2023-03-31 10:40:26 +0800

    fix:#取消无用的注释

--- src/main/java/com/sunset/utils/TokenUtils.java
+++ src/main/java/com/sunset/utils/TokenUtils.java
@@ -5,8 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Date;
 import java.util.HashMap;
