35c57e97f950d736747f2e379b568d0823895821
dillonl <dillonl49@163.com>
2023-03-31 15:22:50 +0800

    feat:#增加用户信息实体类

---
+++ src/main/java/com/sunset/entity/UserInfoEntity.java
@@ -0,0 +1,19 @@
+package com.sunset.entity;
+
+import lombok.Data;
+
+@Data
+public class UserInfoEntity {
+    private String id;
+    private String nickname;
+    private String desc;
+    private String birthday;
+    private int sex;
+    private String height;
+    private String weight;
+    private String waistline;
+    private String showid;
+    private String uid;
+    private String create_time;
+    private String update_time;
+}
