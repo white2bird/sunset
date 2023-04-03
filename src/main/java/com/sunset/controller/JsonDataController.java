e3eb5e3113f6149baab1a7ab30972dddaa7d2a21
dillonl <dillonl49@163.com>
2023-04-03 23:04:00 +0800

    feat:#增加轮播图json数据

--- src/main/java/com/sunset/controller/JsonDataController.java
+++ src/main/java/com/sunset/controller/JsonDataController.java
@@ -1,5 +1,6 @@
 package com.sunset.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sunset.utils.ReturnJson;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+
 @Tag(name = "Goods")
 @RestController
 @RequestMapping("/goods")
@@ -27,11 +30,20 @@ public class JsonDataController {
     public ReturnJson<List> getGoodShopp() throws IOException {
 //        ClassPathResource classPathResource = new ClassPathResource("static/json/goodShopp.json");
 //        InputStream inputStream = classPathResource.getInputStream();
-        File file = new File("src/main/resources/static/json/goodShopp.json");
+        File file = new File("src/main/resources/json/goodShopp.json");
         String jsonData = FileUtils.readFileToString(file);
         List list = JSONObject.parseArray(jsonData); // JSON 转换 List
         return ReturnJson.success(list, "ok");
     }
-
+    @Operation(summary = "首页--轮播图")
+    @GetMapping("/get_banner")
+    public ReturnJson<List> getBannerShopp() throws IOException {
+//        ClassPathResource classPathResource = new ClassPathResource("static/json/goodShopp.json");
+//        InputStream inputStream = classPathResource.getInputStream();
+        File file = new File("src/main/resources/json/banner.json");
+        String jsonData = FileUtils.readFileToString(file);
+        JSONArray list = JSONObject.parseArray(jsonData); // JSON 转换 List
+        return ReturnJson.success(list, "ok");
+    }
 
 }
