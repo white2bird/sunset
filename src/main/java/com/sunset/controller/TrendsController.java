182f40ac12818b5dcb5a83aaeab1dfc9167e5d0f
dillonl <dillonl49@163.com>
2023-04-03 23:19:54 +0800

    feat:#增加轮播图json数据

---
+++ src/main/java/com/sunset/controller/TrendsController.java
@@ -0,0 +1,26 @@
+package com.sunset.controller;
+
+import com.sunset.entity.LoginVerCode;
+import com.sunset.service.SignService;
+import com.sunset.service.TrendsService;
+import com.sunset.utils.ReturnJson;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Trends")
+@RestController
+@RequestMapping("/trends")
+@ResponseBody
+@Slf4j
+public class TrendsController {
+    @Autowired
+    TrendsService trendsService;
+//    @Operation(summary = "动态列表")
+//    @GetMapping("/get/list")
+//    public ReturnJson<String> get_trendsList() {
+//        return trendsService.GetTrendsList();
+//    }
+}
---
+++ src/main/java/com/sunset/service/TrendsService.java
@@ -0,0 +1,13 @@
+package com.sunset.service;
+
+import com.sunset.utils.ReturnJson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class TrendsService {
+//    public ReturnJson<List> GetTrendsList(){
+//
+//    }
+}
