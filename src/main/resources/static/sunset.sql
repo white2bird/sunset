255c702d17f9020ab43f17d74b1b831e130e27ce
dillonl <dillonl49@163.com>
2023-04-11 23:41:49 +0800

    perf:#转存sql

--- src/main/resources/static/sunset.sql
+++ src/main/resources/static/sunset.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2023-04-09 22:20:01
+Date: 2023-04-11 23:41:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `tb_comment` (
   `to_uid` varchar(50) DEFAULT NULL,
   `trends_id` varchar(50) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
-  `start` varchar(255) DEFAULT NULL,
+  `star` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -57,26 +57,27 @@ INSERT INTO `tb_comment` VALUES ('6947FB9B-6979-4871-A67E-DB6D6D192E4D', '828161
 INSERT INTO `tb_comment` VALUES ('BA467980-63BD-4F1D-A3EB-F536B0A2197F', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, '79F48BE2-3551-4A78-A7C4-320324528DCD', ':busstop::busstop::busstop::busstop:', null, '2023-04-09 21:13:11');
 INSERT INTO `tb_comment` VALUES ('3490EC04-9190-4BF1-B04E-C72B1DDD58E6', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, '79F48BE2-3551-4A78-A7C4-320324528DCD', ':oncoming_bus::oncoming_bus::oncoming_bus::oncoming_bus::oncoming_bus::oncoming_automobile::oncoming_automobile::oncoming_taxi:', null, '2023-04-09 21:13:16');
 INSERT INTO `tb_comment` VALUES ('B961C057-84DF-4C77-BE63-4DC561549232', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, '79F48BE2-3551-4A78-A7C4-320324528DCD', ':construction::construction::construction::construction::construction::construction::construction:', null, '2023-04-09 21:13:21');
-INSERT INTO `tb_comment` VALUES ('9A8403B4-2C1B-49B2-979E-4DB7E2D4903D', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, '79F48BE2-3551-4A78-A7C4-320324528DCD', ':man:\r:necktie:\r:jeans:', null, '2023-04-09 21:13:28');
+INSERT INTO `tb_comment` VALUES ('9A8403B4-2C1B-49B2-979E-4DB7E2D4903D', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, '79F48BE2-3551-4A78-A7C4-320324528DCD', ':man:\r:necktie:\r:jeans:', '1', '2023-04-09 21:13:28');
 
 -- ----------------------------
--- Table structure for tb_follow
+-- Table structure for tb_followcomm
 -- ----------------------------
-DROP TABLE IF EXISTS `tb_follow`;
-CREATE TABLE `tb_follow` (
+DROP TABLE IF EXISTS `tb_followcomm`;
+CREATE TABLE `tb_followcomm` (
   `id` varchar(255) NOT NULL,
   `uid` varchar(255) DEFAULT NULL,
-  `following` varchar(255) DEFAULT NULL,
-  `followers` varchar(255) DEFAULT NULL,
-  `star` varchar(255) DEFAULT NULL,
+  `trends_id` varchar(255) DEFAULT NULL,
+  `comment_id` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of tb_follow
+-- Records of tb_followcomm
 -- ----------------------------
-INSERT INTO `tb_follow` VALUES ('0A95E6A6-0B2C-4372-868C-2F7BCFBE03A4', '845412FF-CDEF-4158-8221-BF396F0C404B', '0', null, null);
-INSERT INTO `tb_follow` VALUES ('8281611F-4FA0-4238-BABD-71F46EB25B45', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, null, null);
+INSERT INTO `tb_followcomm` VALUES ('303ED584-0042-4343-9674-41396A3426B2', '8281611F-4FA0-4238-BABD-71F46EB25B88', '79F48BE2-3551-4A78-A7C4-320324528DCD', null, '0', '2023-04-11 21:13:36');
+INSERT INTO `tb_followcomm` VALUES ('752F6BAF-6CC3-4C49-BD15-5B5174EE921A', '8281611F-4FA0-4238-BABD-71F46EB25B88', '79F48BE2-3551-4A78-A7C4-320324528DCD', '9A8403B4-2C1B-49B2-979E-4DB7E2D4903D', '0', '2023-04-11 23:08:39');
 
 -- ----------------------------
 -- Table structure for tb_register
@@ -114,9 +115,28 @@ CREATE TABLE `tb_trends` (
 -- ----------------------------
 -- Records of tb_trends
 -- ----------------------------
-INSERT INTO `tb_trends` VALUES ('79F48BE2-3551-4A78-A7C4-320324528DCD', '845412FF-CDEF-4158-8221-BF396F0C404B', '又开始了:joy:', null, '[\"/images/5ef15519-639a-45ab-b690-4a704338f0a9-1681041141310.jpg\",\"/images/1aa3cf49-2aab-4ae4-b7f0-ca9e18aa10d9-1681041141311.jpg\"]', '2023-04-09 19:52:21');
-INSERT INTO `tb_trends` VALUES ('1DB06575-6DA7-41D0-97E3-F8E1DABD8143', '8281611F-4FA0-4238-BABD-71F46EB25B88', '残灯无焰影幢幢，此夕闻君谪九江。垂死病中惊坐起，暗风吹雨入寒窗。', null, null, '2023-04-09 20:14:16');
+INSERT INTO `tb_trends` VALUES ('79F48BE2-3551-4A78-A7C4-320324528DCD', '845412FF-CDEF-4158-8221-BF396F0C404B', '又开始了:joy:', '1', '[\"/images/5ef15519-639a-45ab-b690-4a704338f0a9-1681041141310.jpg\",\"/images/1aa3cf49-2aab-4ae4-b7f0-ca9e18aa10d9-1681041141311.jpg\"]', '2023-04-09 19:52:21');
+INSERT INTO `tb_trends` VALUES ('1DB06575-6DA7-41D0-97E3-F8E1DABD8143', '8281611F-4FA0-4238-BABD-71F46EB25B88', '残灯无焰影幢幢，此夕闻君谪九江。垂死病中惊坐起，暗风吹雨入寒窗。', '0', null, '2023-04-09 20:14:16');
 INSERT INTO `tb_trends` VALUES ('A832F2EF-C489-470D-B946-B656622A34BF', '845412FF-CDEF-4158-8221-BF396F0C404B', '人生若只如初见，何事秋风悲画扇。____纳兰性德《木兰词·拟古决绝词柬友》', null, '', '2023-04-09 19:47:31');
+
+-- ----------------------------
+-- Table structure for tb_userfollow
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_userfollow`;
+CREATE TABLE `tb_userfollow` (
+  `id` varchar(255) NOT NULL,
+  `uid` varchar(255) DEFAULT NULL,
+  `following` varchar(255) DEFAULT NULL,
+  `followers` varchar(255) DEFAULT NULL,
+  `star` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_userfollow
+-- ----------------------------
+INSERT INTO `tb_userfollow` VALUES ('0A95E6A6-0B2C-4372-868C-2F7BCFBE03A4', '845412FF-CDEF-4158-8221-BF396F0C404B', '0', null, '1');
+INSERT INTO `tb_userfollow` VALUES ('8281611F-4FA0-4238-BABD-71F46EB25B45', '8281611F-4FA0-4238-BABD-71F46EB25B88', null, null, null);
 
 -- ----------------------------
 -- Table structure for tb_userinfo
