4bb27e2e9e3d57b87b005474ca4b06be41239968
dillonl <dillonl49@163.com>
2023-04-16 12:04:31 +0800

    feat:#增加知识社区列表实体类

---
+++ src/main/java/com/sunset/entity/Know/KnowList.java
@@ -0,0 +1,29 @@
+package com.sunset.entity.Know;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class KnowList {
+    private  String id;
+    @Schema(description = "标题")
+    private  String title;
+    @Schema(description = "第三方url")
+    private  String url;
+    @Schema(description = "封面")
+    private  String cover_img;
+    @Schema(description = "内容【富文本】")
+    private  String content;
+    @Schema(description = "类型 【运动，饮食....】")
+    private  String type;
+    @Schema(description = "是否第三方")
+    private  String isthird;
+    @Schema(description = "阅读数")
+    private  String read_num;
+    @Schema(description = "收藏数")
+    private  String like_num;
+    @Schema(description = "评论数")
+    private  String comment_num;
+    @Schema(description = "创建时间")
+    private  String create_time;
+}
