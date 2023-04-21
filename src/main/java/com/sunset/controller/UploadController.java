07494f7c0244876be31831911dcf636a2ad614f8
dillonl <dillonl49@163.com>
2023-04-22 00:28:37 +0800

    fix:#修改打包线上静态资源无法访问bug

--- src/main/java/com/sunset/controller/UploadController.java
+++ src/main/java/com/sunset/controller/UploadController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
@@ -18,8 +19,10 @@ import java.util.*;
 @RequiredArgsConstructor
 @Slf4j
 public class UploadController {
-    String uploadImageUrl = "/src/main/resources/static/images/";
-    String uploadAvatorUrl = "/src/main/resources/static/avator/";
+//    String uploadImageUrl = "/src/main/resources/static/images/";
+//    String uploadAvatorUrl = "/src/main/resources/static/avator/";
+    String uploadImageUrl = "/resources/static/images/";
+    String uploadAvatorUrl = "/resources/static/avator/";
     @Operation(summary = "上传图片【单图】")
     @PostMapping("/image")
     public ReturnJson<Map> UploadImage(@RequestParam("file") MultipartFile fImage) throws IOException {
@@ -29,7 +32,10 @@ public class UploadController {
     @Operation(summary = "上传头像")
     @PostMapping("/avator")
     public ReturnJson<Map> UploadAvator(@RequestParam("file") MultipartFile fImage) throws IOException {
+
         Map map= getImageMap(fImage,uploadAvatorUrl,"/avator/");
+
+
         return ReturnJson.success(map,"ok");
     }
     @Operation(summary = "多图上传【多图】")
@@ -52,7 +58,10 @@ public class UploadController {
         // 重新生成后缀名
         String uuid = UUID.randomUUID() +"-"+ System.currentTimeMillis()+suffixName;
         // 存储的绝对路径
-        String path = System.getProperty("user.dir")+uploadUrl;
+        ApplicationHome h = new ApplicationHome(getClass());
+        File jarF = h.getSource();
+//        String path = System.getProperty("user.dir")+uploadUrl; // 开发环境
+        String path = jarF.getParentFile().toString()+uploadUrl; // 生产环境
         String fuuid = path + uuid;
         HashMap<String,String> map = new HashMap<>();
         map.put("path",setPath+uuid);
